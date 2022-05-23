097c5244ce224ae537f546f5b8e95dd5442e71dc
IMRobonix <imrobotics4@gmail.com>
2022-05-23 12:24:08 +0100

    Final Code

---
+++ app.js
@@ -0,0 +1,36 @@
+ScrollReveal().reveal('.mission', {
+    delay: 500,
+    origin: 'left',
+    distance: '100%'
+});
+ScrollReveal().reveal('.vision', {
+    delay: 500,
+    origin: 'right',
+    distance: '100%'
+});
+ScrollReveal().reveal('.carousel', {
+    delay:500,
+    origin:'bottom',
+    distance: '150%'
+})
+ScrollReveal().reveal('.testimonialTitle', {
+    delay: 500,
+    origin: 'left',
+    distance: '100%'
+});
+ScrollReveal().reveal('.testimony', {
+    delay: 500,
+    origin: 'bottom',
+    distance: '100%'
+});
+ScrollReveal().reveal('.hello-robot', {
+    delay:1000,
+    origin: 'right',
+    distance: '100%',
+})
+ScrollReveal().reveal('.robo', {
+    delay: 1000,
+    origin: 'left',
+    distance: '100%'
+})
+
